db492aa9f724dd162133b8bafbda741c72d30b89
Rishabh Pal <rishabh8931872407@gmail.com>
2026-09-19 00:17:11 +0530

    python ch-2

---
+++ python/datatypes/chapter_02.py
@@ -0,0 +1,16 @@
+# Integers
+
+black_tea_grams = 14
+ginger_grams = 3
+    
+print(black_tea_grams)  # Output: 14
+print(ginger_grams)  # Output: 3
+
+total_grams = black_tea_grams + ginger_grams
+print(total_grams)  # Output: 17
+
+
+is_boiling = True
+stir_count = 5
+total_stir_count = stir_count + is_boiling # Upcasting True to 1, so total_stir_count becomes 6
+print(f"Total stir count: {total_stir_count}")  # Output: Total stir count: 6
